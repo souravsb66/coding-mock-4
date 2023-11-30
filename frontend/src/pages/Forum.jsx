15b9126c3f5a13b7a7b2697055ccd7470dbba54d
Sourav Biswas <sourav.sb56@gmail.com>
2023-11-30 20:01:49 +0530

    Forum Update

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -3,10 +3,13 @@ import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import styled from "styled-components";
 import { baseURL } from "../redux/store";
-import { getForumData, getForumDataFailure, getForumDataSuccess } from "../redux/forum/action";
+import {
+  getForumData,
+  getForumDataFailure,
+  getForumDataSuccess,
+} from "../redux/forum/action";
 
 const Forum = () => {
-  
   const { user } = useSelector((store) => store.authReducer);
   const { forumData } = useSelector((store) => store.forumReducer);
 
@@ -21,27 +24,30 @@ const Forum = () => {
     upvotes: 0,
     answers: [],
     postedDate: "",
-
-  }
+  };
 
   const [modalState, setModalState] = useState(false);
   const [questionData, setQuestionData] = useState(initState);
 
+  const [page, setPage] = useState(1);
+  const [sortOrder, setSortOrder] = useState("");
+  const [filter, setFilter] = useState("");
+
   useEffect(() => {
-    fetchData(`${baseURL}/forum`)
-  },[])
+    fetchData(`${baseURL}/forum`);
+  }, []);
 
   const fetchData = (url) => {
-
     dispatch(getForumData());
-    axios.get(url)
-    .then((res) => {
-      dispatch(getForumDataSuccess(res.data));
-    })
-    .catch((err) => {
-      dispatch(getForumDataFailure());
-    })
-  }
+    axios
+      .get(url)
+      .then((res) => {
+        dispatch(getForumDataSuccess(res.data));
+      })
+      .catch((err) => {
+        dispatch(getForumDataFailure());
+      });
+  };
 
   const openModal = () => {
     setModalState(true);
@@ -132,30 +138,59 @@ const Forum = () => {
         </div>
       </DIV>
 
+      <div>
+        <select name="" id="">
+          <option value="">Sort By Votes</option>
+          <option value="asc">Low to High</option>
+          <option value="desc">High to Low</option>
+        </select>
+
+        <select name="" id="">
+          <option value="">Choose Language</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="Python">Python</option>
+          <option value="Java">Java</option>
+          <option value="Other">Other</option>
+        </select>
+      </div>
+
       <CONTAINERDIV className="questions-container">
-        { forumData.length > 0 && forumData.map((ele) => {
-          return (
-            <div key={ele.id} className="card">
-              <div>
-                <img src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png" alt="" />
-                <p>{ele.username}</p>
-              </div>
-              <div>
-                <h3>{ele.questionTitle}</h3>
+        {forumData.length > 0 &&
+          forumData.map((ele) => {
+            return (
+              <div key={ele.id} className="card">
                 <div>
-                  <span>{ele.language}</span>
-                  <span>Answers {ele.answers.length}</span>
-                  <span>{ele.postedDate}</span>
+                  <img
+                    src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
+                    alt=""
+                  />
+                  <p>{ele.username}</p>
+                </div>
+                <div>
+                  <h3>{ele.questionTitle}</h3>
+                  <div>
+                    <span>{ele.language}</span>
+                    <span>Answers {ele.answers.length}</span>
+                    <span>{ele.postedDate}</span>
+                  </div>
+                </div>
+                <div>
+                  <img
+                    src="https://t3.ftcdn.net/jpg/05/10/38/16/360_F_510381696_jhNwqjLI2W2KDDQVyrEtY9Cucq3ahZhg.jpg"
+                    alt=""
+                  />
+                  <span>{ele.upvotes}</span>
                 </div>
               </div>
-              <div>
-                <img src="https://t3.ftcdn.net/jpg/05/10/38/16/360_F_510381696_jhNwqjLI2W2KDDQVyrEtY9Cucq3ahZhg.jpg" alt="" />
-                <span>{ele.upvotes}</span>
-              </div>
-            </div>
-          )
-        }) }
+            );
+          })}
       </CONTAINERDIV>
+
+      <div className="pagination-container">
+        <button>Prev</button>
+        <button>{page}</button>
+        <button>Next</button>
+      </div>
     </div>
   );
 };
@@ -188,7 +223,7 @@ const DIV = styled.div`
   }
 `;
 
-const CONTAINERDIV = styled.div `
+const CONTAINERDIV = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 2rem;
@@ -204,7 +239,14 @@ const CONTAINERDIV = styled.div `
     img {
       width: 50px;
     }
+
+    span {
+      margin-right: 1rem;
+      padding: 0.2rem 0.4rem;
+      background-color: #b9b9b9;
+      border-radius: 0.3rem;
+    }
   }
-`
+`;
 
 export default Forum;
